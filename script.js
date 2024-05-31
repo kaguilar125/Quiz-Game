cdc48011d11a4a097c5387650be5449e7329c8b6
kaguilar125 <113131954+kaguilar125@users.noreply.github.com>
2024-05-30 22:42:07 -0400

    feiwqjwal

--- script.js
+++ script.js
@@ -1,53 +1,71 @@
-function changeModalImage(imageSrc, element) {
-    var modalImage = document.getElementById("imgb");
+body{
+    background-color: #E8E8E8;
+}
+h1{
+    font-family: "Lilita One", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 128px;
+        color: white;
+        -webkit-text-stroke-width: 6px;
+        -webkit-text-stroke-color: black;
+}
+h2{
+        font-family: "Acme", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+      font-size: 64px;
+}
+.row3{
+    margin-top: 160px;
+}
+.btn{
+    font-family: "Acme", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  font-size: 48px;
+  height: 110px;
+  --bs-btn-border-color: #000 ;
+  --bs-btn-border-width: 6px;
+}
+#btna{
+    text-align: left;
+    background-color: #FF143D;
+    color: white;
     
-
-    var thumbnails = document.querySelectorAll('#grid1 .col-2 img');
-    thumbnails.forEach(function(thumbnail) {
-      thumbnail.classList.remove('active-thumbnail');
-    });
+}
+#btnb{
+    text-align: left;
+    background-color: #3D92FF;
+    color: white;
     
-
-    element.classList.add('active-thumbnail');
+}
+#btnc{
+    text-align: left;
+    background-color: #FED333;
+    color: white;
     
-
-    modalImage.src = imageSrc;
-  }
-
-
-  document.addEventListener("DOMContentLoaded", function() {
-
-    const racingLink = document.getElementById("racingLink");
-    const sportsCarsLink = document.getElementById("sportsCarsLink");
-    const collectionsLink = document.getElementById("collectionsLink");
-    const experiencesLink = document.getElementById("experiencesLink");
-  
-    homeLink.addEventListener("click", function(event) {
-      event.preventDefault(); 
-      navigateToPage("index.html"); 
-    });
-
-    racingLink.addEventListener("click", function(event) {
-      event.preventDefault(); 
-      navigateToPage("racing.html");
-    });
-  
-    sportsCarsLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      navigateToPage("sports_cars.html"); 
-    });
-  
-    collectionsLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      navigateToPage("collections.html");
-    });
-  
-    experiencesLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      navigateToPage("experiences.html"); 
-    });
-  
-    function navigateToPage(url) {
-      window.location.href = url;
-    }
-  });
+}
+#btnd{
+    text-align: left;
+    background-color: #3DE154;
+    color: white;
+    
+}
+#btns{
+    height: 470px;
+    width: 470px;
+    background-color: #932ED1;
+    color: white;
+    
+}
+.btn:hover {
+    color: var(--bs-btn-hover-color);
+    background-color: var(--bs-btn-hover-bg);
+    border-color: #fff;
+}
+.btn-check:checked+.btn, .btn.active, .btn.show, .btn:first-child:active, :not(.btn-check)+.btn:active {
+    color: var(--bs-btn-active-color);
+    background-color: var(--bs-btn-active-bg);
+    border-color: white;
+}
